948657622dbaa19b4ac5f1993564018e7dc62c5b
Jazuck9 <118725070+Jazuck9@users.noreply.github.com>
2023-01-15 23:21:15 +0000

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+use steam
+
+--Lista todas las columnas de la tabla.
+select * from dbo.juegos
+go
+
+--Lista todos los diferentes géneros, eliminando duplicados.
+select distinct(Genre) 
+from dbo.juegos
+order by genre asc
+go
+
+--Lista únicamente aquellos juegos que puedan ser jugados únicamente en Linux
+select nombre
+from dbo.juegos
+where PlatformLinux='true'
+go
+
+--Lista todos los juegos de rol, ordenador por nota.
+select nombre,Metacritic,genre
+from dbo.juegos
+where genre like '%RPG%'
+order by Metacritic desc
+go
+
+--Lista todos aquellos juegos que puedan ser jugados en Español pero no en Frances
+select nombre,SupportedLanguages
+from dbo.juegos
+where SupportedLanguages not like '%french%' and SupportedLanguages like '%spanish%'
+go
